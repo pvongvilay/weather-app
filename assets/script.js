603abed35f253163847ba888030562add205554e
Preston Vongvilay <xprestonxd@gmail.com>
2023-06-12 18:31:50 -0700

    added js code for 5 day forecaset

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,16 @@ var cityEl = document.getElementById('city');
 var windEl = document.getElementById('wind');
 var tempEl = document.getElementById('temp');
 var humidityEl = document.getElementById('humidity');
+var week = document.querySelector('.week');
 function clickHandler(event) {
     var city = searchEl.value
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${api}`)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
         console.log (data)
+        fiveDay(data)
         return data
     })
     .then(function(cords){
@@ -30,6 +32,30 @@ function clickHandler(event) {
         humidityEl.textContent=data.main.humidity
         cityEl.textContent=data.name
     })
+    
 }
 
+function fiveDay (data) {
+var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${api}&units=imperial`
+   fetch(url) 
+   .then(response => {
+    return response.json()
+   })
+   .then(data => {
+    console.log(data)
+     for (var i=0; i < data.list.length; i += 8){
+        var element = data.list[i]
+        console.log (element)
+        var tempEl = document.createElement('p')
+        var windEl = document.createElement('p')
+        var humidityEl = document.createElement('p')
+        var imageEl = document.createElement('img')
+        tempEl.textContent='temp: ' + element.main.temp
+        windEl.textContent='wind: '+ element.wind.speed
+        humidityEl.textContent= element.main.humidity
+        imageEl.setAttribute('src',`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`)
+        week.append(tempEl,windEl,humidityEl,imageEl)
+     }
+   })
+}
 buttonEl.addEventListener('click',clickHandler)
